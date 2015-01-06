9bf02773b7a7db3109f15fe0ee47f4c17c23a021
jun-nakashima <junnakashima@gmail.com>
2015-01-06 11:32:06 +0900

    modify change display text from "ABCD" to "EFGH"

--- src/jp/co/virtualex/jersey/sample/JsonResource.java
+++ src/jp/co/virtualex/jersey/sample/JsonResource.java
@@ -14,10 +14,10 @@ import javax.ws.rs.core.MediaType;
  * 
  * <pre>
  * {
- *     "name1": "AAA",
- *     "name2": "BBB",
- *     "name3": "CCC",
- *     "name4": "DDD"
+ *     "name1": "EEE",
+ *     "name2": "FFF",
+ *     "name3": "GGG",
+ *     "name4": "HHH"
  * }
  * </pre>
  * 
@@ -31,10 +31,10 @@ public class JsonResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public JsonResultDto hello() {
 		JsonResultDto dto = new JsonResultDto();
-		dto.setName1("AAA");
-		dto.setName2("BBB");
-		dto.setName3("CCC");
-		dto.setName4("DDD");
+		dto.setName1("EEE");
+		dto.setName2("FFF");
+		dto.setName3("GGG");
+		dto.setName4("HHH");
 		return dto;
 	}
 }
